90b2a87b1bd124d4864aca3110ecc6e0afabbfb4
Akshaya Parida <akshayaparida2811@gmail.com>
2024-10-28 03:06:39 +0530

    added Quick Links component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,35 +13,35 @@ const config: Config = {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        border: 'hsl(var(--border))', // Custom border color
         card: {
           DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
+          foreground: 'hsl(var(--card-foreground))',
         },
         popover: {
           DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
+          foreground: 'hsl(var(--popover-foreground))',
         },
         primary: {
           DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
           DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
+          foreground: 'hsl(var(--accent-foreground))',
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
+          foreground: 'hsl(var(--destructive-foreground))',
         },
-        border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         chart: {
@@ -49,26 +49,30 @@ const config: Config = {
           '2': 'hsl(var(--chart-2))',
           '3': 'hsl(var(--chart-3))',
           '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))'
-        }
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       fontFamily: {
-        outfit: ['"Outfit"', 'sans-serif'], // Outfit font family
+        outfit: ['"Outfit"', 'sans-serif'],
       },
       fontSize: {
-        'custom-sm': '0.7rem', // Custom font size
-        'custom-small': '12px', // Custom size for 12px
-        'custom-medium': '16px', // Custom size for 16px
+        'custom-sm': '0.7rem',
+        'custom-small': '12px',
+        'custom-medium': '16px',
       },
       lineHeight: {
-        'custom-small': '15.12px', // Custom line height for 12px
-        'custom-medium': '20.16px', // Custom line height for 16px
+        'custom-small': '15.12px',
+        'custom-medium': '20.16px',
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+
+   
+
+
     },
   },
   plugins: [require("tailwindcss-animate")],
